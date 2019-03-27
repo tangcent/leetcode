1c4d4dfe6da2efb5b06a5dbc1bcf7ac4860d6846
tangcent <sweettom@foxmail.com>
2019-03-27 22:43:29 +0800

    TwoSum

---
+++ number/src/main/java/towSum/Solution.java
@@ -0,0 +1,38 @@
+package towSum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * <p>
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complementMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (complementMap.containsKey(num)) {
+                return new int[]{complementMap.get(num), i};
+            } else {
+                complementMap.put(target - num, i);
+            }
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+}
